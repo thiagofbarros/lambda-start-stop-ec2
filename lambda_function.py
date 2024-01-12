a9f4e94262152d753380b1210af00bce1e0355b3
Thiago <thiagofbarros@outlook.com>
2024-01-11 22:00:28 -0300

    updated

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,18 @@
 import boto3
 
-region = 'us-east-1'
-ec2 = boto3.resource('ec2', region_name=region)
+ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
     action = event["action"]
 
-    instances = ec2.instances.filter(Filters = [{'Name': 'tag:AutoStartStop', 'Values': ['true']}])
+    instances = ec2.instances.filter(
+        Filters = [
+            {
+                'Name': 'tag:AutoStartStop',
+                'Values': ['true']
+            }
+        ]
+    )
 
     for instance in instances:
         if action == 'Start':     
